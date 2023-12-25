bd97ec649e71c543b4ea2d97884055c49e85f2a6
Harshal Patil <107706576+harshal010710@users.noreply.github.com>
2023-12-26 02:17:08 +0530

    Add files via upload

---
+++ src6.4/app/demo/demo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  standalone: true,
+  imports: [],
+  templateUrl: './demo.component.html',
+  styleUrl: './demo.component.css'
+})
+export class DemoComponent 
+{
+  public Name = "Marvellous";
+}
